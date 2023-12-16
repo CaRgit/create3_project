9902c5071c1f2c8c6542fe75aac7646cd9166b71
alvmorcal <112963358+alvmorcal@users.noreply.github.com>
2023-12-16 15:01:32 +0100

    Update proy_path_copy.py

--- proy_path_copy.py
+++ proy_path_copy.py
@@ -228,6 +228,8 @@ def main(args=None):
         cv2.waitKey(1)
         
     goal = click_coordinates[0]
+    print(start)
+    print(goal)
 
     img_with_path, nodes, _, _ = rrt_star(img, start, goal, step_size_cm, max_iterations, rewiring_radius_cm, robot_radius)
 
