88a202bd42c89f0d611e33eb8c621f5c1902ccd1
alvmorcal <112963358+alvmorcal@users.noreply.github.com>
2023-12-17 15:40:10 +0100

    Update ir.py

--- ir.py
+++ ir.py
@@ -11,11 +11,34 @@ class IR(Node):
     def __init__(self):
         super().__init__("Infrared_Node")
         self.subscription = self.create_subscription(IrIntensityVector,'/ir_intensity', self.ir_callback, QoSProfile(depth=10, reliability=ReliabilityPolicy.BEST_EFFORT))
-
+        self.ir_1 = []
+        self.ir_2 = []
+        self.ir_3 = []
+        self.ir_4 = []
+        self.ir_5 = []
+        self.ir_6 = []
+        self.ir_7 = []
+        
     def ir_callback(self, msg):
-        for reading in msg.readings:
+        for i, reading in enumerate(msg.readings):
             intensity_value = reading.value
-            self.get_logger().info(f'Intensidad: {intensity_value}')
+            if i == 0:
+                self.ir_1.append(intensity_value)
+            elif i == 1:
+                self.ir_2.append(intensity_value)
+            elif i == 2:
+                self.ir_3.append(intensity_value)
+            elif i == 3:
+                self.ir_4.append(intensity_value)
+            elif i == 4:
+                self.ir_5.append(intensity_value)
+            elif i == 5:
+                self.ir_6.append(intensity_value)
+            elif i == 6:
+                self.ir_7.append(intensity_value)
+                
+            self.get_logger().info(f'Lectura {i + 1}: Intensidad: {intensity_value}')
+
         
 def main(args=None):
     rclpy.init(args=args)
