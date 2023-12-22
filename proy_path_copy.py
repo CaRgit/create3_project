d0ec8362256269ac1275ddddfea61f1471d773da
alvmorcal <112963358+alvmorcal@users.noreply.github.com>
2023-12-22 16:31:19 +0100

    Update proy_path_copy.py

--- proy_path_copy.py
+++ proy_path_copy.py
@@ -51,7 +51,7 @@ class GoToGoal(Node):
 
     def go_to_goal(self):
         goal = Odometry()
-        goal.pose.pose.position.x, goal.pose.pose.position.y = self.path[self.current_goal_index]
+        goal.pose.pose.position.y, goal.pose.pose.position.x = self.path[self.current_goal_index]
         new_vel = Twist()
 
         distance_to_goal = math.hypot(goal.pose.pose.position.x - self.odom.pose.pose.position.x, goal.pose.pose.position.y - self.odom.pose.pose.position.y)
