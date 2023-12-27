50cb1cef82cd395d31b864af6c0390ea84903a06
alvmorcal <112963358+alvmorcal@users.noreply.github.com>
2023-12-28 00:00:44 +0100

    Update rrt.py

--- rrt.py
+++ rrt.py
@@ -120,8 +120,8 @@ def main():
     img_path = f'./{"mapa.png"}'
     img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
 
-    step_size_cm = float(10) #input("Ingrese el tamaño del paso (en cm): "))
-    max_iterations = int(1000)
+    step_size_cm = float(15) #input("Ingrese el tamaño del paso (en cm): "))
+    max_iterations = int(500)
     diametro_robot = int(5) #input("Ingrese el diametro del robot (en cm): "))
 
     cv2.imshow("Mapa", img)
