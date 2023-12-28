f5ad5fc0f2c9042801c0ea4cd3768950bf63917f
alvmorcal <112963358+alvmorcal@users.noreply.github.com>
2023-12-28 15:07:12 +0100

    Update rrt.py

--- rrt.py
+++ rrt.py
@@ -113,7 +113,7 @@ def rrt_star(img, start, goal, step_size_cm, max_iter, diametro_robot):
             cv2.line(img_with_path, (node.x, node.y), (node.parent.x, node.parent.y), (0, 255, 0), 1)
             cv2.circle(img_with_path, (node.x, node.y), 3, (0, 0, 255), -1)  
         for node in nodos_simp:
-            cv2.line(img_with_path, (node.x, node.y), (node[-1].x, node[-1].y), (0, 255, 0), 2)
+            cv2.line(img_with_path, (node.x, node.y), (node.x, node.y), (0, 255, 0), 2)
             cv2.circle(img_with_path, (node.x, node.y), 5, (0, 0, 255), -1)   
             
         return img_with_path, nodos_simp
