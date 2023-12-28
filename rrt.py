c79376fae8979dc01a2c92a29114bbe4e921be8a
alvmorcal <112963358+alvmorcal@users.noreply.github.com>
2023-12-28 14:43:43 +0100

    Update rrt.py

--- rrt.py
+++ rrt.py
@@ -105,14 +105,13 @@ def rrt_star(img, start, goal, step_size_cm, max_iter, diametro_robot):
             nodos.insert(0, current_node)
             current_node = current_node.parent
 
-        nodos_simp=simplify_path(nodes, img, diametro_robot)
+        nodos_simp=simplify_path(nodos, img, diametro_robot)
         
         for node in nodos_simp:
             cv2.line(img_with_path, (node.x, node.y), (node.parent.x, node.parent.y), (0, 255, 0), 2)
             cv2.circle(img_with_path, (node.x, node.y), 5, (0, 0, 255), -1)   
             
         return img_with_path, nodos_simp
-
 
 def save_path_to_txt(nodes, filename, scale=0.01):
     with open(filename, 'w') as file:
