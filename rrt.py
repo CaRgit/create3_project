8e0a9d17b17f8c5b67111428b2496c24e1992f77
alvmorcal <112963358+alvmorcal@users.noreply.github.com>
2023-12-28 16:55:15 +0100

    Update rrt.py

--- rrt.py
+++ rrt.py
@@ -31,11 +31,13 @@ def simplify_path(nodes, img, diametro_robot):
     while i < len(nodes) - 1:
         current_node = nodes[i]
         next_node = nodes[i + 1]
-        while i < len(nodes) and not has_collision(img, current_node.x, current_node.y, next_node.x, next_node.y, diametro_robot):
-            next_node = nodes[i]
+        while i < len(nodes) - 1 and not has_collision(img, current_node.x, current_node.y, next_node.x, next_node.y, diametro_robot):
+            next_node = nodes[i + 1]
             i += 1
-        simplified_nodes.append(nodes[i-1])
+        simplified_nodes.append(nodes[i])
+        i += 1
     return simplified_nodes
+
 
 def mouse_callback(event, x, y, flags, param):
     if event == cv2.EVENT_LBUTTONUP:
