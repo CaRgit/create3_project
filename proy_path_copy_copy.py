8dfe4b7084cdc3111d2041b7d75507b15ab17a21
alvmorcal <112963358+alvmorcal@users.noreply.github.com>
2023-12-29 15:54:28 +0100

    Update proy_path_copy_copy.py

--- proy_path_copy_copy.py
+++ proy_path_copy_copy.py
@@ -118,7 +118,7 @@ class GoToGoal(Node):
             self.last_lightring = lightring
 
             ### PRUEBA CON AUDIO ###
-            self.audio_publisher.publish(audio_msg)
+            self.audio_publisher.publish(self.audio_msg)
             ### PRUEBA CON AUDIO ###
 
         else:
