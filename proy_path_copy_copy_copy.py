830718d73bffaa0c24382a2ab7ef2ddd2b7f59eb
joqg04 <152640801+joqg04@users.noreply.github.com>
2024-01-12 18:01:52 +0100

    Update proy_path_copy_copy_copy.py

--- proy_path_copy_copy_copy.py
+++ proy_path_copy_copy_copy.py
@@ -2,65 +2,68 @@ import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
 from nav_msgs.msg import Odometry
+from irobot_create_msgs.msg import LedColor
+from irobot_create_msgs.msg import LightringLeds
 from irobot_create_msgs.msg import IrIntensityVector
+from irobot_create_msgs.msg import AudioNoteVector
+from irobot_create_msgs.msg import AudioNote
 from rclpy.qos import ReliabilityPolicy, QoSProfile
 import math
 import cv2
 import numpy as np
 import random
 import time
-from irobot_create_msgs.msg import LedColor
-from irobot_create_msgs.msg import LightringLeds
 
-from irobot_create_msgs.msg import AudioNoteVector
+### GET INITIAL POSITION ###
 
-class GoToGoalInitializer(Node):
+class GetInitialPosition(Node):
     def __init__(self):
-        super().__init__("GoToGoalInitializerNode")
+        super().__init__("GetInitialPosition")
         self.subscription = self.create_subscription(Odometry, '/odom', self.odom_callback, QoSProfile(depth=10, reliability=ReliabilityPolicy.BEST_EFFORT))
-        #self.initial_position_set = False
         self.start_time = time.time()
 
     def odom_callback(self, data):
         current_time = time.time()
-        while not (current_time - self.start_time) >= 1:
+        self.get_logger().info(f"Getting initial position...")
+        while not (current_time - self.start_time) >= 2:
             time.sleep(0.1)
             current_time = time.time()
         self.initial_position = (data.pose.pose.position.x, data.pose.pose.position.y)
         self.get_logger().info(f"Initial position set: {self.initial_position}")
 
+### GO TO GOAL ###
+
 class GoToGoal(Node):
     def __init__(self, points):
         super().__init__("GoToGoalNode")
+        
         self.cmd_vel_pub = self.create_publisher(Twist, '/cmd_vel', 10)
+        self.lights_publisher = self.create_publisher(LightringLeds, '/cmd_lightring', 10)
+        self.audio_publisher = self.create_publisher(AudioNoteVector, '/cmd_audio', 10)
         self.subscription = self.create_subscription(Odometry, '/odom', self.odom_callback, QoSProfile(depth=10, reliability=ReliabilityPolicy.BEST_EFFORT))
         self.subscription = self.create_subscription(IrIntensityVector,'/ir_intensity', self.ir_callback, QoSProfile(depth=10, reliability=ReliabilityPolicy.BEST_EFFORT))
+        
         self.timer = self.create_timer(0.1, self.go_to_goal)
+        
         self.odom = Odometry()
+        self.last_lightring = LightringLeds()
+        self.last_lightring.override_system = True 
+        ### PRUEBA CON AUDIO ###
+        self.audio_msg = AudioNoteVector()
+        self.audio_msg.append = True 
+        notes_data = [(523, 0.2), (523, 0.2), (523, 0.2), (523, 0.5), (415, 0.5), (466, 0.5), (523, 0.3), (466, 0.2), (523, 1)]# Frecuencia y duración (segundos) para la nota
+        for frequency, duration in notes_data:
+            note = AudioNote()
+            note.frequency = frequency
+            #note.max_runtime.nanosec = int(duration * 1000000)
+            note.max_runtime.sec = duration
+            self.audio_msg.notes.append(note)
+        ### PRUEBA CON AUDIO ###
+        
         self.path = points
-        self.media = 0
         self.ir=[]
         self.current_goal_index = 0
         self.end_of_goals = False
-
-        self.lights_publisher = self.create_publisher(LightringLeds, 'cmd_lightring', 10)
-        self.last_lightring = LightringLeds()
-        self.last_lightring.override_system = False
-
-                
-        self.sound_publisher = self.create_publisher(AudioNoteVector, '/cmd_audio', 10)
-        self.Musica = AudioNoteVector[] 
-        self.Musica.append(523,3)
-        #self.Musica.append(523,0.2)
-        #self.Musica.append(523,0.2)
-        #self.Musica.append(523,0.2)
-        #self.Musica.append(523,0.5)
-        #self.Musica.append(415,0.5)
-        #self.Musica.append(466,0.5)
-        #self.Musica.append(523,0.3)
-        #self.Musica.append(466,0.2)
-        #self.Musica.append(523,1)
-
 
     def odom_callback(self, data):
         self.odom = data
@@ -70,15 +73,15 @@ class GoToGoal(Node):
         for reading in msg.readings:
             intensity_value = reading.value
             self.ir.append(intensity_value)
-        #self.media = np.mean([self.ir[2], self.ir[3], self.ir[4]])    
+        #Para acceder a los valores ir por separado: ([self.ir[2], self.ir[3], self.ir[4]], ...)    
 
     def go_to_goal(self):
         goal = Odometry()
         goal.pose.pose.position.y, goal.pose.pose.position.x = self.path[self.current_goal_index]
         new_vel = Twist()
 
-        cp = ColorPalette()
         lightring = self.last_lightring
+        cp = ColorPalette()
 
         distance_to_goal = math.hypot(goal.pose.pose.position.x - self.odom.pose.pose.position.x, goal.pose.pose.position.y - self.odom.pose.pose.position.y)
         angle_to_goal = math.atan2(goal.pose.pose.position.y - self.odom.pose.pose.position.y, goal.pose.pose.position.x - self.odom.pose.pose.position.x)
@@ -92,11 +95,11 @@ class GoToGoal(Node):
         elif angle_error < -math.pi:
             angle_error += 2 * math.pi
 
-        kp_ang, kp_lin = 10, 5
+        kp_ang, kp_lin = 15, 4
 
         if abs(distance_to_goal) > distance_tolerance:
             new_vel.angular.z = kp_ang * angle_error
-            new_vel.linear.x = (1 - abs(angle_error)*2 / math.pi) * kp_lin * distance_to_goal
+            new_vel.linear.x = float(max(0.0, (1 - abs(angle_error)*2 / math.pi) * kp_lin * distance_to_goal))
         else:
             self.current_goal_index += 1
             self.get_logger().info(f"Goal {self.current_goal_index} reached")
@@ -109,28 +112,36 @@ class GoToGoal(Node):
             self.get_logger().info(f"End of the goal list ({self.current_goal_index})")
             self.end_of_goals = True
 
-            self.sound_publisher.publish(self.Musica)
-
-        if any(lectura > 600 for lectura in self.ir): #self.media >= 250:
-            new_vel.linear.x = 0.0
-            new_vel.angular.z = 0.0
-            print('ESTORBAAAS')
-
             lightring = LightringLeds()
             lightring.override_system = True
-            lightring.leds = [LedColor(red=255,green=0,blue=0), LedColor(red=255,green=0,blue=0), LedColor(red=255,green=0,blue=0), LedColor(red=255,green=0,blue=0), LedColor(red=255,green=0,blue=0), LedColor(red=255,green=0,blue=0)]
+            lightring.leds = [cp.green, cp.green, cp.green, cp.green, cp.green, cp.green]
             self.last_lightring = lightring
+
+            ### PRUEBA CON AUDIO ###
+            self.audio_publisher.publish(self.audio_msg)
+            ### PRUEBA CON AUDIO ###
+
+        else:
+            if any(lectura > 600 for lectura in self.ir): 
+                new_vel.linear.x = 0.0
+                new_vel.angular.z = 0.0
+                print('Obstacle detected')
+
+                lightring = LightringLeds()
+                lightring.override_system = True
+                lightring.leds = [cp.red, cp.red, cp.red, cp.red, cp.red, cp.red]
+                self.last_lightring = lightring
             
-        elif(self.last_lightring.override_system == True):
-            lightring = LightringLeds()
-            lightring.override_system = True
-            lightring.leds = [LedColor(red=0,green=0,blue=0), LedColor(red=0,green=0,blue=0), LedColor(red=0,green=0,blue=0), LedColor(red=0,green=0,blue=0), LedColor(red=0,green=0,blue=0), LedColor(red=0,green=0,blue=0)]
-            self.last_lightring = lightring
+            elif(self.last_lightring.override_system == True):
+                lightring = LightringLeds()
+                lightring.override_system = True
+                lightring.leds = [cp.blue, cp.blue, cp.blue, cp.blue, cp.blue, cp.blue]
+                self.last_lightring = lightring
 
         self.cmd_vel_pub.publish(new_vel)
-
         self.lights_publisher.publish(lightring)
 
+### GET PATH (RRT STAR) ###
 
 class RRTStarNode:
     def __init__(self, x, y):
@@ -138,66 +149,108 @@ class RRTStarNode:
         self.parent = None
         self.cost = 0.0
 
-def is_valid_point(img, x, y, robot_radius):
-    mask = cv2.circle(np.zeros_like(img, dtype=np.uint8), (x, y), robot_radius, 255, thickness=1)
+def is_valid_point(img, x, y, diametro_robot):
+    mask = cv2.circle(np.zeros_like(img, dtype=np.uint8), (x, y), int(diametro_robot), 255, thickness=1)
     return not np.any(img[mask == 255] == 0) and 0 <= x < img.shape[1] and 0 <= y < img.shape[0] and img[y, x] != 0
 
 def nearest_node(nodes, x, y):
-    distances = [(node.x - x) ** 2 + (node.y - y) ** 2 for node in nodes]
+    distances = np.sqrt((np.array([node.x for node in nodes]) - x)**2 + (np.array([node.y for node in nodes]) - y)**2)
     return nodes[np.argmin(distances)]
 
 def new_point(x_rand, y_rand, x_near, y_near, step_size):
     theta = math.atan2(y_rand - y_near, x_rand - x_near)
     return x_near + step_size * math.cos(theta), y_near + step_size * math.sin(theta)
 
-def has_collision(img, x1, y1, x2, y2, robot_radius):
+def has_collision(img, x1, y1, x2, y2, diametro_robot):
     points = np.column_stack((np.linspace(x1, x2, 100), np.linspace(y1, y2, 100)))
-    return any(not is_valid_point(img, int(x), int(y), robot_radius) for x, y in points)
+    return any(not is_valid_point(img, int(x), int(y), diametro_robot) for x, y in points)
 
-def rrt_star(img, start, goal, step_size_cm, max_iter, rewiring_radius_cm, robot_radius):
-    nodes, img_with_path, points = [RRTStarNode(*start)], np.copy(img), []
-    goal_node = None
+def simplify_path(nodes, img, diametro_robot):
+    simplified_nodes = [nodes[0]]  
+    i=0
+    #for i in range(1, len(nodes)):
+    while (i<len(nodes)):
+        current_node = simplified_nodes[-1]
+        next_node = nodes[i]
+        while i < len(nodes) and not has_collision(img, current_node.x, current_node.y, next_node.x, next_node.y, diametro_robot):
+            i += 1
+            if i < len(nodes):
+                next_node = nodes[i]
+            else:
+                break
+        if i < len(nodes):
+            simplified_nodes.append(nodes[i-1])
+    simplified_nodes.append(nodes[-1])
+    return simplified_nodes
+        
+def rrt_star(img, start, goal, step_size_cm, max_iter, diametro_robot):
+    nodes = [RRTStarNode(*start)]
+    img_with_path = np.copy(img)
+    goal_reached = False
 
     for _ in range(max_iter):
-        x_rand, y_rand = random.randint(0, img.shape[1] - 1), random.randint(0, img.shape[0] - 1)
+        if random.uniform(0, 1) < 0.2:
+            x_rand = random.uniform(max(0, goal[0] - 50), min(img.shape[1] - 1, goal[0] + 50))
+            y_rand = random.uniform(max(0, goal[1] - 50), min(img.shape[0] - 1, goal[1] + 50))
+        else:
+            x_rand, y_rand = random.randint(0, img.shape[1] - 1), random.randint(0, img.shape[0] - 1)
+
         nearest = nearest_node(nodes, x_rand, y_rand)
         x_new, y_new = new_point(x_rand, y_rand, nearest.x, nearest.y, step_size_cm)
 
-        if is_valid_point(img, int(x_new), int(y_new), robot_radius):
+        if is_valid_point(img, int(x_new), int(y_new), diametro_robot):
             node_new = RRTStarNode(int(x_new), int(y_new))
-            near_nodes = [node for node in nodes if math.hypot(node.x - node_new.x, node.y - node_new.y) < rewiring_radius_cm]
-            min_cost_node = nearest_node(near_nodes, x_new, y_new)
 
-            if not has_collision(img, min_cost_node.x, min_cost_node.y, node_new.x, node_new.y, robot_radius):
-                node_new.parent = min_cost_node
-                node_new.cost = min_cost_node.cost + math.hypot(node_new.x - min_cost_node.x, node_new.y - min_cost_node.y)
-
-                for near_node in near_nodes:
-                    new_cost = node_new.cost + math.hypot(node_new.x - near_node.x, node_new.y - near_node.y)
-                    if new_cost < near_node.cost and not has_collision(img, node_new.x, node_new.y, near_node.x, near_node.y, robot_radius):
-                        near_node.parent, near_node.cost = node_new, new_cost
+            if not has_collision(img, nearest.x, nearest.y, node_new.x, node_new.y, diametro_robot):
+                node_new.parent = nearest
+                node_new.cost = nearest.cost + math.sqrt((node_new.x - nearest.x)**2 + (node_new.y - nearest.y)**2)
 
                 nodes.append(node_new)
-                cv2.line(img_with_path, (min_cost_node.x, min_cost_node.y), (node_new.x, node_new.y), (200, 200, 200), 1)
 
-                if not points and not has_collision(img, node_new.x, node_new.y, goal[0], goal[1], robot_radius):
-                    goal_node = RRTStarNode(*goal)
-                    goal_node.parent = node_new
-                    goal_node.cost = node_new.cost + math.hypot(goal_node.x - node_new.x, goal_node.y - node_new.y)
-                    nodes.append(goal_node)
-                    cv2.line(img_with_path, (node_new.x, node_new.y), (goal_node.x, goal_node.y), (0, 255, 0), 2)
+                cv2.circle(img_with_path, (node_new.x, node_new.y), 1, (0, 0, 255), -1)
+                cv2.line(img_with_path, (node_new.x, node_new.y), (node_new.parent.x, node_new.parent.y), (0, 255, 0), 1)
+                
+                if not has_collision(img, node_new.x, node_new.y, goal[0], goal[1], diametro_robot) and ((math.sqrt((goal[0] - node_new.x)**2 + (goal[1] - node_new.y)**2)) <= step_size_cm):
+                    if not goal_reached:
+                        penult_nodo = node_new
+                        coste_total = node_new.cost + math.sqrt((goal[0] - node_new.x)**2 + (goal[1] - node_new.y)**2)
+                        goal_reached = True
+                    if (node_new.cost + math.sqrt((goal[0] - node_new.x)**2 + (goal[1] - node_new.y)**2)) < coste_total:
+                        penult_nodo = node_new
+                        coste_total = node_new.cost + math.sqrt((goal[0] - node_new.x)**2 + (goal[1] - node_new.y)**2)
+                   
+    if goal_reached:
+        goal_node = RRTStarNode(*goal)
+        goal_node.parent = penult_nodo
+        goal_node.cost = coste_total
+        nodes.append(goal_node)
 
-                if goal_node in nodes:
-                    current_node = goal_node
-                    while current_node.parent is not None:
-                        cv2.line(img_with_path, (current_node.x, current_node.y), (current_node.parent.x, current_node.parent.y), (0, 255, 0), 2)
-                        points.append((float(current_node.x * 0.01), float(current_node.y * 0.01)))
-                        current_node = current_node.parent
-                    points.reverse()
+        start_node = RRTStarNode(*start)
+        start_node.parent = None
+        start_node.cost = 0.0
 
-                    return img_with_path, points, start, goal
+        nodos = []
+        current_node = goal_node
+        while current_node.parent is not None:
+            nodos.insert(0, current_node)
+            current_node = current_node.parent
+        nodos.insert(0, start_node)
+        
+        nodos_simp=simplify_path(nodos, img, diametro_robot)
+        
+        for node in nodos:
+            if node.parent is not None:
+                cv2.line(img_with_path, (node.x, node.y), (node.parent.x, node.parent.y), (0, 255, 0), 2)
+            cv2.circle(img_with_path, (node.x, node.y), 2, (0, 0, 255), -1)  
+        for i in range(1, len(nodos_simp)):
+            cv2.line(img_with_path, (nodos_simp[i - 1].x, nodos_simp[i - 1].y), (nodos_simp[i].x, nodos_simp[i].y), (0, 255, 0), 3)
+            cv2.circle(img_with_path, (nodos_simp[i].x, nodos_simp[i].y), 3, (0, 0, 255), -1)
 
-    return img_with_path, points, start, goal
+        path=[]
+        for nodo_simp in nodos_simp:
+            path.append((float(nodo_simp.x * 0.01), float(nodo_simp.y * 0.01)))
+            
+        return img_with_path, path
 
 def mouse_callback(event, x, y, flags, params):
     if event == cv2.EVENT_LBUTTONUP:
@@ -210,6 +263,8 @@ def draw_marker_on_image(img_with_markers, label, point):
     cv2.putText(img_with_markers, label, (int(point[0]) + 10, int(point[1]) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
     return img_with_markers
 
+### MAIN FUNCTION ###
+
 def main(args=None):
     rclpy.init(args=args)
     first = True
@@ -218,19 +273,16 @@ def main(args=None):
 
     img_path = './mapa.png'
     img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
-    step_size_cm = float(input("Enter step size (in cm): "))
-    max_iterations = 1000000
-    rewiring_radius_cm = float(input("Enter rewiring radius (in cm): "))
-    robot_radius = int(input("Enter robot radius (in cm): "))
-    robot_radius = robot_radius + 5
-    
+    step_size_cm = 40 #float(input("Enter step size (in cm): "))
+    max_iterations = 200 #int(input("Max iterations for RRT star: "))
+    robot_diameter = 40 #int(input("Enter robot diameter (in cm): "))
 
     while end_program:            
         if first or (choice == 'C'):
             if (choice == 'C'):
                 rclpy.init(args=args)
                 
-            initializer = GoToGoalInitializer()
+            initializer = GetInitialPosition()
             rclpy.spin_once(initializer)
             initializer.destroy_node()
             start = (int(initializer.initial_position[1] * 100), int(initializer.initial_position[0] * 100))
@@ -245,22 +297,15 @@ def main(args=None):
                 cv2.waitKey(1)
             goal = goal[0]
         
-            img_with_path, trajectory, _, _ = rrt_star(img, start, goal, step_size_cm, max_iterations, rewiring_radius_cm, robot_radius)
+            img_with_path, nodes = rrt_star(img, start, goal, step_size_cm, max_iterations, robot_diameter)
         
             draw_marker_on_image(img_with_path, 'start', start)
             draw_marker_on_image(img_with_path, 'goal', goal)
             cv2.imshow("Map RRT*", img_with_path)
             cv2.waitKey(1)
             cv2.imwrite("final_solution.png", img_with_path, [int(cv2.IMWRITE_PNG_COMPRESSION), 9])
-
-            
-            #minimal_publisher = GoToGoal(trajectory)
-            #rclpy.spin(minimal_publisher)
-            #print('HOLA')
-            #minimal_publisher.destroy_node()
-            #rclpy.shutdown()
         
-            minimal_publisher = GoToGoal(trajectory)
+            minimal_publisher = GoToGoal(nodes)
             while rclpy.ok() and not minimal_publisher.end_of_goals:
                 rclpy.spin_once(minimal_publisher, timeout_sec=0.1)
             minimal_publisher.destroy_node()
@@ -292,6 +337,7 @@ class ColorPalette():
         self.purple = LedColor(red=127,green=0,blue=255)
         self.white = LedColor(red=255,green=255,blue=255)
         self.grey = LedColor(red=189,green=189,blue=189)
+        self.default = LedColor(red=1,green=1,blue=1)
 
 class Lights():
     """ Class to tell the robot to set lightring lights as part of dance sequence"""
