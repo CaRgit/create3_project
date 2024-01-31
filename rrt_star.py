e993231f7e50c2745b5cb16bd8fabd3616b05ffa
alvmorcal <112963358+alvmorcal@users.noreply.github.com>
2024-02-01 00:45:01 +0100

    Update rrt_star.py

--- rrt_star.py
+++ rrt_star.py
@@ -27,7 +27,7 @@ def has_collision(img, x1, y1, x2, y2, diametro_robot):
 
 def simplify_path(nodes, img, diametro_robot):
     i = 0
-    simplified_nodes = []
+    simplified_nodes = [nodes[0]]
     
     while i < len(nodes) - 1:
         current_node = nodes[i]
