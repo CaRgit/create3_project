d455d4569006f0bcd0876bf249973a6b5d71a121
alvmorcal <112963358+alvmorcal@users.noreply.github.com>
2024-01-31 16:31:14 +0100

    Update rrt.py

--- rrt.py
+++ rrt.py
@@ -32,10 +32,11 @@ def simplify_path(nodes, img, diametro_robot):
     while (i<len(nodes)):
         current_node = nodes[i]
         next_node = nodes[i]
+        i += 1
         while i < len(nodes) and not has_collision(img, current_node.x, current_node.y, next_node.x, next_node.y, diametro_robot):
-            i += 1
             next_node = nodes[i]
-        simplified_nodes[j]=nodes[i-2]
+            i += 1
+        simplified_nodes[j]=nodes[i-1]
         j += 1    
     #simplified_nodes.append(nodes[i-1])
     return simplified_nodes
