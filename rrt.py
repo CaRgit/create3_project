2841ea112a58755838049a0e02956e2b89b8baf1
alvmorcal <112963358+alvmorcal@users.noreply.github.com>
2024-01-31 16:55:12 +0100

    Update rrt.py

--- rrt.py
+++ rrt.py
@@ -26,20 +26,19 @@ def has_collision(img, x1, y1, x2, y2, diametro_robot):
     return any(not is_valid_point(img, int(x), int(y), diametro_robot) for x, y in points)
 
 def simplify_path(nodes, img, diametro_robot):
-    i=0
-    simplified_nodes=[nodes[i]]
-    while (i<len(nodes)):
+    i = 0
+    simplified_nodes = [nodes[i]]
+    
+    while i < len(nodes) - 1:
         current_node = nodes[i]
-        i += 1
-        if i < len(nodes):
-            next_node = nodes[i]
-        
-        while i < len(nodes) and not has_collision(img, current_node.x, current_node.y, next_node.x, next_node.y, diametro_robot):
+        next_node = nodes[i + 1]
+
+        while i < len(nodes) - 1 and not has_collision(img, current_node.x, current_node.y, next_node.x, next_node.y, diametro_robot):
             i += 1
-            if i < len(nodes):
-                next_node = nodes[i]
-                
-        simplified_nodes.append(nodes[i-1])
+            next_node = nodes[i]
+
+        simplified_nodes.append(nodes[i])
+        i += 1
     
     return simplified_nodes
 
