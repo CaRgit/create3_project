c38c00941fc70224f4b7d4053d427572c9ba1fc0
alvmorcal <112963358+alvmorcal@users.noreply.github.com>
2024-02-01 01:04:03 +0100

    Update project.py

--- project.py
+++ project.py
@@ -174,19 +174,21 @@ def has_collision(img, x1, y1, x2, y2, diametro_robot):
 
 def simplify_path(nodes, img, diametro_robot):
     i = 0
-    simplified_nodes = [nodes[0]]
+    simplified_nodes = [nodes[i]]
     
-    while i < len(nodes) - 1:
-        current_node = nodes[i]
-        next_node = nodes[i + 1]
+    while i < (len(nodes)):
+        current_node = simplified_nodes[-1]
+        next_node = nodes[i]
 
-        while i < len(nodes) - 1 and not has_collision(img, current_node.x, current_node.y, next_node.x, next_node.y, diametro_robot):
+        while (i < len(nodes)) and not has_collision(img, current_node.x, current_node.y, next_node.x, next_node.y, diametro_robot):
             i += 1
-            next_node = nodes[i]
-
-        simplified_nodes.append(nodes[i-1])
-        i += 1
-    simplified_nodes.append(nodes[i-1])
+            if i < len(nodes):
+                next_node = nodes[i]
+            else:
+                break
+        if i < len(nodes):
+            simplified_nodes.append(nodes[i-1])
+    simplified_nodes.append(nodes[-1])
     return simplified_nodes
         
 def rrt_star(img, start, goal, step_size_cm, max_iter, diametro_robot):
